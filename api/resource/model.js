a4f3e76a2f1febc51fda1efdbe5feb12bface848
quinn5moli <quinn.moli@gmail.com>
2021-11-21 13:44:20 -0800

    models started

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,23 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig')
+
+const find = () => {
+    return db('resources')
+}
+
+const findById = async (resource_id) => {
+    const resource = await db('resources').where({ resource_id }).first()
+
+    return resource
+}
+
+const create = async (newResource) => {
+    const [resource_id] = await db('resources').insert(newResource)
+
+    return findById(resource_id)
+}
+
+module.exports = {
+    find,
+    findById,
+    create
+}
--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,37 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig')
+
+const find = () => {
+    return db('tasks as t')
+    .leftJoin('projects as p', 't.project_id', 'p.project_id')
+    .select('t.task_id', 't.task_description', 't.task_notes', 'p.project_name', 'p.project_description')
+}
+
+const findById = async task_id => {
+    const task = await db('tasks').where({ task_id }).first()
+
+    if(task.task_completed === 1) {
+        task.task_completed = true
+    } else {
+        task.task_completed = false
+    }
+
+    return task
+}
+
+const create = async (newTask) => {
+    if(newTask.task_completed === true || newTask.task.task_completed === 1) {
+        newTask.task_completed = 1
+    } else {
+        newTask.task_completed = 0
+    }
+
+    const [task_id] = await db('tasks').inser(newTask)
+
+    return findById(task_id)
+}
+
+module.exports = {
+    find,
+    findById,
+    create
+}
