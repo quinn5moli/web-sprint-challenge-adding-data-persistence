d20e73c0cd659ba00422c7ea12bc01df6ea61f72
quinn5moli <quinn.moli@gmail.com>
2021-11-21 12:08:05 -0800

    index.js and server setup

--- api/server.js
+++ api/server.js
@@ -1,1 +1,23 @@
-// build your server here and require it from index.js
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const morgan = require('morgan');
+
+
+const ProjectsRouter = require('./project/router');
+const ResourcesRouter = require('./resource/router');
+const TasksRouter = require('./task/router');
+
+const server = express();
+
+server.use(helmet())
+server.use(cors())
+server.use(morgan('dev'))
+server.use(express.json())
+
+server.use(express.json());
+server.use('/api/projects', ProjectsRouter);
+server.use('/api/resources', ResourcesRouter);
+server.use('/api/tasks', TasksRouter)
+
+module.exports = server;
